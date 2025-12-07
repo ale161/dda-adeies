acf4618be846651ba263d69914ba39c485567508
ale161 <ale161@users.noreply.github.com>
2025-12-07 00:59:45 +0000

    Deploying to gh-pages from @ ale161/dda-adeies@026169197f3bfdb71549c44a91bf20ff2f755b55 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/dda-adeies/sw.js', { scope: '/dda-adeies/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
